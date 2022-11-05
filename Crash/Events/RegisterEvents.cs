631cba770c80d72f9c44a2c5939f2b524ec965d4
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2022-11-05 12:32:39 -0700

    Update RegisterEvents.cs

--- Crash/Events/RegisterEvents.cs
+++ Crash/Events/RegisterEvents.cs
@@ -8,14 +8,12 @@ namespace Crash.Events
 
 		internal static void RegisterEvents()
 		{
-			DeRegisterEvents(); // Remove all events first just in case.
-								// TODO: [Structure] all registered Event are static functions, maybe it is fine now but it could bring challenges once we try to do something different according to the Branch data
+			// Remove all events first just in case.
+			DeRegisterEvents();
 			RhinoDoc.AddRhinoObject += AddItem.Event;
 			RhinoDoc.DeleteRhinoObject += RemoveItem.Event;
 			RhinoDoc.SelectObjects += SelectItem.Event;
 			RhinoDoc.DeselectObjects += SelectItem.Event;
-
-
 		}
 
 		internal static void DeRegisterEvents()
@@ -24,7 +22,6 @@ namespace Crash.Events
 			RhinoDoc.DeleteRhinoObject -= RemoveItem.Event;
 			RhinoDoc.SelectObjects -= SelectItem.Event;
 			RhinoDoc.DeselectObjects -= SelectItem.Event;
-
 		}
 
 	}

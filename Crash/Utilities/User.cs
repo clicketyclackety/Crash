c26af0a1de5783f6af42bbb6c19a39404337214d
Callum <csykes@structurecraft.com>
2022-11-05 15:29:01 -0700

    Updated pipeline

--- Crash/Utilities/User.cs
+++ Crash/Utilities/User.cs
@@ -2,22 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Security.Cryptography;
+using System.Drawing;
 
 namespace Crash.Utilities
 {
+
     public class User
     {
-    public string name;
-    public Color color;
+        public string name;
+        public Color color;
 
-    public UserColor(string inputName)
-    {
-        name = inputName;
-        var md5 = MD5.Create();
-        var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(name));
-        color = Color.FromArgb(hash[0], hash[1], hash[2]);
-
-    }
+        public static Color UserColor(string inputName)
+        {
+            var name = inputName;
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(name));
+            Color color = Color.FromArgb(hash[0], hash[1], hash[2]);
+            return color;
+        }
 
     }
 

41a92d5e8093a6a12613fb16448e0423ec44d55d
Callum <csykes@structurecraft.com>
2022-11-05 12:40:26 -0700

    Created command wrappers

---
+++ Crash/Commands/OpenSharedModel.cs
@@ -0,0 +1,34 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+
+namespace Crash.Commands
+{
+
+    public sealed class OpenSharedModel : Command
+    {
+
+        public OpenSharedModel()
+        {
+            Instance = this;
+        }
+
+        
+        public static OpenSharedModel Instance { get; private set; }
+
+        
+        public override string EnglishName => "OpenSharedModel";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            
+            return Result.Success;
+        }
+
+    }
+
+}
---
+++ Crash/Commands/Release.cs
@@ -0,0 +1,33 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+
+namespace Crash.Commands
+{
+
+    public sealed class ReleaseCommand : Command
+    {
+
+        public ReleaseCommand()
+        {
+            Instance = this;
+        }
+
+        public static ReleaseCommand Instance { get; private set; }
+
+        
+        public override string EnglishName => "Release";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            
+            return Result.Success;
+        }
+
+    }
+
+}

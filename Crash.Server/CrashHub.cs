3463c58c833718dffaeb7f1e92a116a4fd6e64c7
Curtis Wensley <curtis.wensley@gmail.com>
2022-11-05 16:23:53 -0400

    Initial server and database implementation

---
+++ Crash.Server/CrashHub.cs
@@ -0,0 +1,37 @@
+﻿using Crash.Server.Model;
+using Microsoft.AspNetCore.SignalR;
+using SpeckLib;
+
+namespace Crash.Server
+{
+
+    public interface ICrashClient
+    {
+        Task ReceiveMessage(string user, Speck speck);
+    }
+
+    public class CrashHub : Hub<ICrashClient>
+    {
+        CrashContext _context;
+        public CrashHub(CrashContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SendMessage(string user, Speck speck)
+            => await Clients.All.ReceiveMessage(user, speck);
+
+        public async Task Poke()
+        {
+            var speck = new Speck();
+            _context.Specks.Add(speck);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            // send 3dm and current state??
+
+            return base.OnConnectedAsync();
+        }
+    }
+}

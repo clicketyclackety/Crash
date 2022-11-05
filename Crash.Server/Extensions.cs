bf19e52f255611952c0ffb6123caa4fe65aa1984
Curtis Wensley <curtis.wensley@gmail.com>
2022-11-05 17:15:17 -0400

    Add database migrations

---
+++ Crash.Server/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crash.Server
+{
+    public static class Extensions
+    {
+        public static IHost MigrateDatabase<T>(this IHost webHost) where T : DbContext
+        {
+            SQLitePCL.Batteries.Init();
+            var serviceScopeFactory = (IServiceScopeFactory?)webHost
+                .Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var dbContext = services.GetRequiredService<T>();
+                dbContext.Database.Migrate();
+            }
+
+            return webHost;
+        }
+    }
+}
--- Crash.Server/Program.cs
+++ Crash.Server/Program.cs
@@ -4,18 +4,38 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.DependencyInjection;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-var databaseFile = "Databse.db";
+var databasePath = "App_Data";
+var databaseFile = Path.Combine(databasePath, "Database.db");
+
+if (!Directory.Exists(databasePath))
+{
+    Directory.CreateDirectory(databasePath);
+}
+
 builder.Services.AddSignalR();
 
 builder.Services.AddDbContext<CrashContext>(options =>
-               options.UseSqlite($"Data Source={databaseFile}"));
+               options.UseSqlite($"Data Source={databaseFile.Replace("\\", "/")}"));
 
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapHub<CrashHub>("/Crash");
 
+
+app.MigrateDatabase<CrashContext>();
+
+//var crashContext = app.Services.<CrashContext>();
+//var speck = new SpeckLib.Speck();
+//crashContext.Specks.Add(speck);
+//crashContext.SaveChanges();
+
+
 app.Run();
+
+

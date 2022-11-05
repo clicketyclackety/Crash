aef799b771937c6d4c74a72ca4c05473de8fab5f
rfeathers068 <rfeathers@thorntontomasetti.com>
2022-11-05 19:52:52 -0400

    DB queries

--- Crash.Server/CrashHub.cs
+++ Crash.Server/CrashHub.cs
@@ -21,7 +21,6 @@ namespace Crash.Server
             _context = context;
         }
 
-
         public async Task Add(string user, Speck speck)
         {
             try
@@ -36,31 +35,92 @@ namespace Crash.Server
             await Clients.Others.Add(user, speck);
         }
 
-
         public async Task Update(string user, Guid id, Speck speck)
         {
-            var oldSpeck = _context.Specks.FirstOrDefault(r => r.Id == id);
-
+            try
+            {
+                _context.Specks.Remove(_context.Specks.FirstOrDefault(r => r.Id == id));
+                _context.Specks.Add(Model.Speck.From(speck));
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+            }
+            await Clients.Others.Update(user, id, speck);
         }
 
         public async Task Delete(string user, Guid id)
         {
-
+            try
+            {
+                _context.Specks.Remove(_context.Specks.FirstOrDefault(r => r.Id == id));
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+            }
+            await Clients.Others.Delete(user, id);
         }
 
         public async Task Done(string user)
         {
+            try
+            {
+                List<Model.Speck> done = new List<Model.Speck>();
+                foreach (var speck in _context.Specks)
+                {
+                    if (speck.LockedBy == user)
+                    {
+                        speck.Temporary = false;
+                        speck.LockedBy = null;
+                        done.Add(speck);
+                    }
+                }
+                _context.Specks.UpdateRange(done);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+            }
+            await Clients.Others.Done(user);
 
         }
 
         public async Task Select(string user, Guid id)
         {
-
+            try
+            {
+                Model.Speck modSpec = _context.Specks.FirstOrDefault(r => r.Id == id);
+                modSpec.Temporary = true;
+                modSpec.LockedBy = user;
+                _context.Specks.Update(modSpec);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+            }
+            await Clients.Others.Select(user, id);
         }
 
         public async Task Unselect(string user, Guid id)
         {
-
+            try
+            {
+                Model.Speck unSelect = _context.Specks.FirstOrDefault(r => r.Id == id);
+                unSelect.Temporary = false;
+                unSelect.LockedBy = null;
+                _context.Specks.Update(unSelect);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+            }
+            await Clients.Others.Unselect(user, id);
         }
 
 

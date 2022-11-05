bca4862705647e96e4ff5edb0e8aabd37264d411
Morteza Karimi <morteza@mcneel.com>
2022-11-05 19:16:07 -0400

    Add start command

---
+++ Crash/Commands/StartSharedModel.cs
@@ -0,0 +1,33 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+
+namespace Crash.Commands
+{
+
+    public sealed class StartSharedModel : Command
+    {
+
+        public StartSharedModel()
+        {
+            Instance = this;
+        }
+
+        
+        public static StartSharedModel Instance { get; private set; }
+
+        
+        public override string EnglishName => "StartSharedModel";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            return Result.Success;
+        }
+
+    }
+
+}

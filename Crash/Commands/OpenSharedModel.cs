0c9231cffce50e703f298dd3a7c595e37297faed
Callum <csykes@structurecraft.com>
2022-11-05 15:53:22 -0700

    Renamed to RequestMAnager

--- Crash/Commands/OpenSharedModel.cs
+++ Crash/Commands/OpenSharedModel.cs
@@ -26,7 +26,7 @@ namespace Crash.Commands
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            Reconciliation.StartOrContinueLocalClient();
+            RequestManager.StartOrContinueLocalClient();
 
 
             return Result.Success;

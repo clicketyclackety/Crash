b1fcddb309770c277f2f801373543b1503ffbef8
Callum <csykes@structurecraft.com>
2022-11-22 13:47:01 -0800

    Created initial compiler and action

---
+++ Scripts/YakCompiler.ps1
@@ -0,0 +1,37 @@
+param (
+    [Parameter(Mandatory=$true, HelpMessage="The input directory")]
+    [Alias("Path")]
+    [string]$inputDir,
+
+    [Parameter(Mandatory=$false, HelpMessage="The output location")]
+    [Alias("DestinationPth")]
+    [string]$outputDir = $inputDir,
+
+    [Parameter(Mandatory=$false, HelpMessage="What Config was Project built for?")]
+    [Alias("Config")]
+    [string]$configuration="Debug",
+
+    [Parameter(Mandatory=$false, HelpMessage="What OS is current running")]
+    [string]$os="win",
+
+    [Parameter(Mandatory=$false, HelpMessage="Minimum required version of Rhino")]
+    [Alias("Version")]
+    [string]$rhinoVersion="7.21",
+
+    [Parameter(Mandatory=$false, HelpMessage="Push to yak server?")]
+    [bool]$publish=$false
+)
+
+$formattedVersion = $rhinoVersion.Replace('.','_')
+$crashVersion = "1.0.0"
+
+$fileName = "Crash"
+$yakFile = "$outputDir\\crash-$crashVersion-rh$formattedVersion-$os.yak"
+
+if (Test-Path $yakFile)
+{
+    Remove-Item $yakFile
+}
+
+Compress-Archive -Path "$inputDir\*.*" -DestinationPath $yakFile -CompressionLevel Optimal
+Compress-Archive -Path "$inputDir\Server" -DestinationPath $yakFile -CompressionLevel Optimal -Update

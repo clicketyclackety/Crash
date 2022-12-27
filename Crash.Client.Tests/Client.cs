c1f02758a7b3fecff4982e43fb3124c3454387b5
Callum <csykes@structurecraft.com>
2022-12-27 18:07:43 +0000

    Fixed faiing test

--- Crash.Client.Tests/Client.cs
+++ Crash.Client.Tests/Client.cs
@@ -18,7 +18,9 @@
         [TestMethod]
         public async Task Select()
         {
-            CrashClient client = new CrashClient("Me", new Uri("http://localhost:8080/Crash"));
+
+            CrashClient client = new CrashClient("Me", new Uri("http://localhost:5000/Crash"));
+            await client.StartAsync();
             await client.Select(Guid.Empty);
         }
 

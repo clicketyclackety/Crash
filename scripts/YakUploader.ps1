0c862f7a01d7104714c0e34a8b58f77b8a9c08e2
Callum <csykes@structurecraft.com>
2022-12-27 23:01:03 +0000

    Improved YakUploadeer

--- scripts/YakUploader.ps1
+++ scripts/YakUploader.ps1
@@ -8,19 +8,18 @@ param (
 
 $script_dir = $PSScriptRoot
 $base_dir = (get-item $script_dir).parent.FullName
-$buildDir = "$base_dir\Crash\bin\$configuration\net48\"
+$buildDir = "$base_dir\src\Crash\bin\$configuration\net48\"
 
 $loc = Get-Location
 Set-Location $buildDir
 
 # Build plugin
-& dotnet build -c $configuration "$base_dir/Crash/Crash.csproj"
+& dotnet build -c $configuration "$base_dir/src/Crash/Crash.csproj"
 
 # Build servers
 & dotnet publish -c $configuration -r win-x64 "$base_dir/Crash.Server/Crash.Server.csproj" /p:Publish=True
 # & dotnet publish -c $configuration -r osx-x64 "$base_dir/Crash.Server/Crash.Server.csproj" /p:Publish=True
 # & dotnet publish -c $configuration -r osx-arm64 "$base_dir/Crash.Server/Crash.Server.csproj" /p:Publish=True
-
 
 $yakexe = "C:\Program Files\Rhino 7\System\Yak.exe"
 & $yakexe build --platform win

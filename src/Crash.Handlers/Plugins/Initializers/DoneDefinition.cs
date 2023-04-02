b2c4820f5dc862e7c1a1c634099fde64856e899d
Callum <csykes@structurecraft.com>
2023-04-02 14:44:33 -0700

    Created Done definition

---
+++ src/Crash.Handlers/Plugins/Initializers/DoneDefinition.cs
@@ -0,0 +1,37 @@
+﻿using Rhino.Display;
+using Rhino.Geometry;
+
+namespace Crash.Handlers.Plugins.Initializers
+{
+
+	public sealed class DoneDefinition : IChangeDefinition
+	{
+		public Type ChangeType => typeof(Change);
+
+		public string ChangeName => $"{nameof(Crash)}.Done";
+
+		public IEnumerable<IChangeCreateAction> CreateActions { get; }
+
+		public IEnumerable<IChangeRecieveAction> RecieveActions { get; }
+
+
+		public DoneDefinition()
+		{
+			CreateActions = Array.Empty<IChangeCreateAction>();
+			RecieveActions = new List<IChangeRecieveAction> { new DoneRecieve() };
+		}
+
+
+		public void Draw(DrawEventArgs drawArgs, DisplayMaterial material, IChange change)
+		{
+			throw new NotImplementedException();
+		}
+
+		public BoundingBox GetBoundingBox(IChange change)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+}

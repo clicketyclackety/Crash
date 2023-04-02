87d46820d09cd35c9a1f5ddcaaaa9c05d2d11906
Callum <csykes@structurecraft.com>
2023-04-02 13:20:36 -0700

    Created Plugin Interfaces

---
+++ src/Crash.Handlers/Plugins/IChangeCreateAction.cs
@@ -0,0 +1,19 @@
+﻿namespace Crash.Handlers.Plugins
+{
+
+	/// <summary>Describes how to Convert an Event into a Change</summary>
+	public interface IChangeCreateAction
+	{
+
+		/// <summary>The Action this ICreateAction responds to</summary>
+		ChangeAction Action { get; }
+
+		/// <summary>Attempts to convert an Event into an IChange</summary>
+		public bool TryConvert(object sender, CreateRecieveArgs crashArgs, out IEnumerable<IChange> changes);
+
+		/// <summary>Tests Event Args for Conversion</summary>
+		public bool CanConvert(object sender, CreateRecieveArgs crashArgs);
+
+	}
+
+}
